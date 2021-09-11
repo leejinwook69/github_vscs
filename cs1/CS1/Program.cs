556a992571775307da6383d8bb0d8bfb870bf247
leejinwook69 <80254252+leejinwook69@users.noreply.github.com>
2021-09-12 01:13:43 +0900

    b

---
+++ cs1/CS1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CS1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("sc unity " + "test");
+        }
+    }
+}
