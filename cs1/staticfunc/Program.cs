4846c8a71f452d1e5550b8825bd8068823606df6
leejinwook69 <80254252+leejinwook69@users.noreply.github.com>
2021-09-14 04:04:41 +0900

    static

---
+++ cs1/staticfunc/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+public class Player
+{
+
+    public static void PVP(Player _Left, Player _Right)
+    {
+        _Left.HP -= _Right.HP;
+        _Right.HP -= _Left.HP;
+    }
+
+    private int HP = 100;
+    void Damage(int _Damage)
+    {
+        HP -= _Damage;
+    }
+}
+namespace staticfunc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player Newplayer = new Player();
+            
+        }
+    }
+}
