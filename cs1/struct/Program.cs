ddf9172ab7fd1832f12100eabe60bea216235cef
leejinwook69 <80254252+leejinwook69@users.noreply.github.com>
2021-09-17 02:50:49 +0900

    struct

---
+++ cs1/struct/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _struct
+{
+    struct struct1 
+    {
+        public int a;
+        public int b;
+
+        void set()
+        {
+            a = 10;
+            b = 10;
+        }
+    }
+
+
+    class Program
+{
+
+        static void proc(struct1 str)
+        {
+            str.a -= 3;
+        }
+
+        static void Main(string[] args)
+    {
+            struct1 str1 = new struct1();
+
+            str1.a = 12;
+            str1.b = 12;
+
+            proc(str1);
+        }
+
+}
+}
